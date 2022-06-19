ab0577899330bb322906d79149d2e51c91067ce4
Abhijeet Shukla <ashukla3@atlassian.com>
2022-06-20 03:47:54 +0530

    Updates

---
+++ precache-manifest.da567bbbd970fb3357d2fc8f2c6c5feb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e1d84fc147e3918cb3",
+    "url": "/static/css/main.290c27a2.chunk.css"
+  },
+  {
+    "revision": "13e1d84fc147e3918cb3",
+    "url": "/static/js/main.13e1d84f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "f276566d7bf025a92ddbe1bdb1cf6c03",
+    "url": "/index.html"
+  }
+];
